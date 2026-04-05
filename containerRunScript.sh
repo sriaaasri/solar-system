77d30b9e4c94b450879ba91b113272b5dac18544
sri_satya <91309599+sriaaasri@users.noreply.github.com>
2026-04-05 13:17:53 +0530

    Added run command in script

--- containerRunScript.sh
+++ containerRunScript.sh
@@ -6,3 +6,14 @@ echo "MONGO_URI = $MONGO_URI"
 echo "MONGO_PASSWORD = $MONGO_PASSWORD"
 
 docker images
+
+echo "Running solar-system image"
+
+docker run -d -p 3000:3000 \
+              -e MONGO_URI=$MONGO_URI \
+              -e MONGO_USERNAME=$MONGO_USERNAME \
+              -e MONGO_PASSWORD=$MONGO_PASSWORD \
+               chowdary2001/solar-system:e53c12b24c4384099c0c4c2576c96f5d0bebd7d4 
+
+echo testing contianer status
+wget -q -O - localhost:3000/live | grep live
